569e186ffc70bec78b263080e3467c5746beb049
anasazy <anasazy@web.de>
2020-05-14 14:08:02 +0200

    Router configuration fixed + unused imports removed

--- homepanel/src/app/app.module.ts
+++ homepanel/src/app/app.module.ts
@@ -1,32 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
-
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
-
-
-import { LightControlpanelModule } from './light-controlpanel/light-controlpanel.module';
-
-
-const routes: Routes = [
- {path: '', redirectTo: '/light', pathMatch: 'full'}
-  ];
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    LightControlpanelModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
