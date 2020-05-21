512bfceb729cf4e2779ba5fa874d3bcdbd73d6c4
Marcel Rudolph <anasazy@web.de>
2020-05-21 13:40:54 +0200

    NgRx Store added

--- homepanel/src/app/app.module.ts
+++ homepanel/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +20,13 @@ import { AppComponent } from './app.component';
 		BrowserModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
+
+		StoreModule.forRoot({}, {}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+		EffectsModule.forRoot([]),
+
 		AppRoutingModule,
+
 		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
 	],
 	providers: [],
